8acdba3cfa8f1408c193268a6a7a27d21a7f5180
GeoN5 <sgepyh2916@naver.com>
2019-01-30 04:46:45 +0900

    Google login interlock implement

--- app/src/main/java/com/example/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -1,18 +1,27 @@
 package com.example.instagram
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    var auth : FirebaseAuth? = null //로그인 관리
+    private var auth : FirebaseAuth? = null //로그인 관리
+    private var googleSigninClient : GoogleSignInClient? = null
+    private val GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +31,14 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             createAndLoginEmail()
         }
+        google_sign_int_button.setOnClickListener {
+            googleLogin()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("553186965299-5c53fidh1pvani35ct5hd2avabt49rcu.apps.googleusercontent.com").requestEmail().build()
+        googleSigninClient = GoogleSignIn.getClient(this,gso)
+
     }
 
     private fun createAndLoginEmail() { //Authentication
@@ -62,6 +79,27 @@ class LoginActivity : AppCompatActivity() {
         if(user != null){
             startActivity(Intent(this,MainActivity::class.java))
             finish()
+        }
+    }
+
+    private fun googleLogin(){
+        val signInIntent = googleSigninClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth?.signInWithCredential(credential)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_LOGIN_CODE && resultCode == Activity.RESULT_OK){
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                firebaseAuthWithGoogle(result.signInAccount!!)
+            }
         }
     }
 
