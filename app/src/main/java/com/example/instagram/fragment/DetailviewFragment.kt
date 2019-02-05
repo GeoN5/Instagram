8f54dfd5fdb4753980ff76d13e59c66343a26b4e
GeoN5 <sgepyh2916@naver.com>
2019-02-06 02:30:38 +0900

    Detailfragment Recyclerview basic set

--- app/src/main/java/com/example/instagram/fragment/DetailviewFragment.kt
+++ app/src/main/java/com/example/instagram/fragment/DetailviewFragment.kt
@@ -2,14 +2,39 @@ package com.example.instagram.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instagram.R
+import kotlinx.android.synthetic.main.fragment_detail.view.*
 
 class DetailviewFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail,container,false)
+        val view =  inflater.inflate(R.layout.fragment_detail,container,false)
+        view.detailviewfragment_recyclerview.adapter = DetailRecyclerviewAdapter()
+        view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(context)
+        return view
     }
+
 }
+
+class DetailRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail,parent,false)
+        return CustomViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+}
+
+class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
